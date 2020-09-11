99e324811a935c5ae505069ff263b546177a699b
DeeDowns <dee_downs@outlook.com>
2020-09-11 12:44:20 -0400

    addProject and POST request

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,9 +1,15 @@
 const db = require('../data/db-config')
 
 module.exports = {
-    getProjects
+    getProjects,
+    addProjects
 }
 
 function getProjects() {
     return db('projects')
+}
+
+function addProjects(project) {
+    return db('projects')
+    .insert(project, 'id')
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -16,4 +16,17 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    const projectData = req.body
+    Projects.addProjects(projectData)
+    .then(newProject => {
+        console.log(newProject)
+        res.status(201).json({ message: 'new project created' })
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    })
+})
+
 module.exports = router
